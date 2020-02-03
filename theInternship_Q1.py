167981810074fc7179f5415f42ba169ca0cec31c
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2020-02-04 02:49:32 +0700

    Update theInternship_Q1.py

--- theInternship_Q1.py
+++ theInternship_Q1.py
@@ -1,5 +1,17 @@
+"""
+Sorting Acronyms, The Internship Program 2020 first question
+Created By Thaweesak Saiwongse (Note) 03/02/2020
+"""
+
 def acronym(buffer):
-    acronym =""
+    """ acronym function. to look for the first character of each word in string
+    Args:
+        buffer (string) input from user
+
+    Returns:
+        acronym for each line of input
+    """
+    acronym ="" 
     for word in buffer.split():
         if(word[0].isupper()==True):
             acronym=acronym+word[0]
@@ -10,9 +22,9 @@ def main():
     ans = list()
     for i in range(loop):
         buffer = input("")
-        ans.append(acronym(buffer))
-        ans.sort()
-        ans.sort(key=len, reverse=True)
+        ans.append(acronym(buffer))             
+        ans.sort()                              #alphabetically sort
+        ans.sort(key=len, reverse=True)         #sort by length
     print(*ans,sep="\n")
 if __name__ == '__main__':
     main()
