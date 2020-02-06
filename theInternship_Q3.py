6ae5258b3a98d67348073243969cdb324323d6db
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2020-02-06 23:18:34 +0700

    Complete Digit Hangman
    Add final comment

--- theInternship_Q3.py
+++ theInternship_Q3.py
@@ -1,23 +1,26 @@
-
+"""
+Digit Hangman, The Internship Program 2020 third question
+Created By Thaweesak Saiwongse (Note) 06/02/2020
+"""
 def main():
     hangmanRaw = input("")
     hangman = hangmanRaw.split(" ")
-    result = ['_']*12
-    wrong = list()
-    pos = list()
+    result = ['_']*12 #list of result
+    wrong = list() #list for collecting wrong guess
+    pos = list() # list of correct guess indexes
     score =0
     print(*result, sep=" ")
     for i in range(5):
         ans = input("")
-        for j in range(len(hangman)):
-            if hangman[j]==ans:
-                pos.append(j)
-        if pos!=[]:
+        for j in range(len(hangman)): 
+            if hangman[j]==ans: #guess answer is correct
+                pos.append(j) #add the index to the list
+        if pos!=[]: #if the guess is wrong
             for k in range(len(pos)):
-                result[pos[k]]=ans
-                score+=1
+                result[pos[k]]=ans #add correct guess to show in result
+                score+=1 #update score
         else:
-            wrong.append(ans)
+            wrong.append(ans) #add incorrect guess to wrong list
         print(*result,*wrong)
         pos.clear()
     print(score)
