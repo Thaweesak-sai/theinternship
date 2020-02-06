4561a7faab8b24f038436dda9f661e178a31edec
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2020-02-06 15:43:52 +0700

    Done. But needed to split into functions
    เสร็จแล้วแต่ว่ายังไม่เช็ค+อยากแยกฟังชั่น

--- theInternship_Q3.py
+++ theInternship_Q3.py
@@ -3,10 +3,23 @@ def main():
     hangmanRaw = input("")
     hangman = hangmanRaw.split(" ")
     result = ['_']*12
+    wrong = list()
+    pos = list()
+    score =0
     print(*result, sep=" ")
-    # print(hangman)
-    # print(hangman[0], hangman[2])
     for i in range(5):
         ans = input("")
+        for j in range(len(hangman)):
+            if hangman[j]==ans:
+                pos.append(j)
+        if pos!=[]:
+            for k in range(len(pos)):
+                result[pos[k]]=ans
+                score+=1
+        else:
+            wrong.append(ans)
+        print(*result,*wrong)
+        pos.clear()
+    print(score)
 if __name__ == '__main__':
     main()
