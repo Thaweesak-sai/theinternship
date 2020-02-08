1c1e610d7a0a9d1fe96a27c3e26f5207276c2bb9
Thaweesak Saiwongse <57885091+Thaweesak-sai@users.noreply.github.com>
2020-02-08 10:54:52 +0700

    Complete basic-webCrawler

--- crawler.js
+++ crawler.js
@@ -1,8 +1,13 @@
+/** *
+*   crawler.js 
+*   Created By Thaweesak Saiwongse (Note) 07/02/2020
+*/
+
 const company = require("./web-crawler.js");
 
 const main = async () => {
-  const companies = await company();
-  const sorted = companies.sort((a, b) => -(a.description - b.description));
+  const companies = await company(); //call module web-crawler.js
+  const sorted = companies.sort((a, b) => -(a.description - b.description)); //sort by length of text by ascending
   const output = sorted.map(e => e.companyLogo);
   const END_POINT = "https://theinternship.io/";
   const parsed = output.map(e => ({
--- web-crawler.js
+++ web-crawler.js
@@ -1,3 +1,8 @@
+/** *
+*   web-crawler.js 
+*   Created By Thaweesak Saiwongse (Note) 07/02/2020
+*/
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 const siteUrl = "https://theinternship.io/"
@@ -8,11 +13,11 @@ const fetchData = async ()=>{
 };
 
 const getCompany = async()=>{
-    const array = [];
+    const array = [];                   //array for keeping data 
     const $ = await fetchData();
-    $('div.partner').each(function(){
-        let companyLogo = $(this).find('a>img').attr('src');
-        let description = $(this).find('span').text();
+    $('div.partner').each(function(){   //iterate through webpage to div.partner
+        let companyLogo = $(this).find('a>img').attr('src');        //look for <a><img src="...">
+        let description = $(this).find('span').text();              // look for <span>
         array.push({companyLogo,description,});
     });
     return array;
